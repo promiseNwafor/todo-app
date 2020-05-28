a50501762995d8a68b603e78b63f2addfd9d62cd
Promise Nwafor <makybeky9@gmail.com>
2020-05-28 18:27:27 +0100

    initial

---
+++ build/precache-manifest.1afc96500aa80a51c80bddaf26238f4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5762d701d0755319d1ad719c1bc7da45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16c6fac7fd94e3a4cbb0",
+    "url": "/static/css/main.4315d77b.chunk.css"
+  },
+  {
+    "revision": "36d55f678fa4a628d66a",
+    "url": "/static/js/2.5c9e5c35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5c9e5c35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16c6fac7fd94e3a4cbb0",
+    "url": "/static/js/main.a1bd7b18.chunk.js"
+  },
+  {
+    "revision": "68eeff9f8f0013407352",
+    "url": "/static/js/runtime-main.bd2b2ea9.js"
+  }
+]);
